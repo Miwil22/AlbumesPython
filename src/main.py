594b60eae3273585149c727c401be654d5481243
Miwil22 <mgm22796@gmail.com>
2026-01-29 17:41:42 +0100

    todo ok

--- src/main.py
+++ src/main.py
@@ -3,34 +3,33 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
-from database import obtener_albumes
 
 app = FastAPI()
 
 basedir = os.path.dirname(os.path.abspath(__file__))
 
 app.mount("/static", StaticFiles(directory=os.path.join(basedir, "static")), name="static")
+
 templates = Jinja2Templates(directory=os.path.join(basedir, "templates"))
+
 
 @app.get("/")
 def ver_web(request: Request):
     return templates.TemplateResponse("index.html", {
         "request": request, 
-        "titulo": "Inicio - NEXUS"
+        "titulo": "Inicio"
     })
 
 @app.get("/albumes") 
 def ver_albumes(request: Request):
-    lista_albumes = obtener_albumes()
-    
     return templates.TemplateResponse("albumes/albumes.html", {
         "request": request,
-        "albumes": lista_albumes
+        "albumes": [] 
     })
 
 @app.get("/api/info")
 def ver_api():
-    return {"mensaje": "API conectada a MySQL Localhost"}
+    return {"mensaje": "Esta es la API REST funcionando"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
