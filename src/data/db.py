56036b0aaf0325cc70a103ad6045f5bead34a976
Miwil22 <mgm22796@gmail.com>
2026-01-29 17:00:23 +0100

    algunos errores

--- src/data/db.py
+++ src/data/db.py
@@ -1,37 +1,77 @@
-from dotenv import load_dotenv
-import os
-from sqlmodel import create_engine, Session, SQLModel
-from models.album import Album
+import mysql.connector
 
-load_dotenv()
+db_config = {
+    'host': '127.0.0.1',      
+    'user': 'root',          
+    'password': '',   
+    'database': 'albumes_db'  
+}
 
-db_user: str = os.getenv("DB_USER", "miguel")
-db_password: str = os.getenv("DB_PASSWORD", "1234")
-db_server: str = os.getenv("DB_SERVER", "fastapi-db")
-db_port: str = os.getenv("DB_PORT", "3306")
-db_name: str = os.getenv("DB_NAME", "albumesdb")
-
-DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
-engine = create_engine(os.getenv("DB_URL", DATABASE_URL), echo=True)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-def init_db():
-    # Borra las tablas y las crea de nuevo (Reset completo)
-    SQLModel.metadata.drop_all(engine)
-    SQLModel.metadata.create_all(engine)
+def get_connection():
+    temp_config = db_config.copy()
+    del temp_config['database']
     
-    with Session(engine) as session:
-        session.add(Album(nombre="POST HUMAN: NEX GEN", artista="Bring Me The Horizon", genero="Alt Metal / Electronic", fecha_lanzamiento="2024-05-24"))
-        session.add(Album(nombre="The Death of Peace of Mind", artista="Bad Omens", genero="Industrial / Metalcore", fecha_lanzamiento="2022-02-25"))
-        session.add(Album(nombre="Scoring The End of the World", artista="Motionless in White", genero="Industrial Metal", fecha_lanzamiento="2022-06-10"))
-        session.add(Album(nombre="Take Me Back To Eden", artista="Sleep Token", genero="Prog / Alt Metal", fecha_lanzamiento="2023-05-19"))
-        session.add(Album(nombre="Paper Hearts", artista="Sleep Theory", genero="Alt Rock / Metal", fecha_lanzamiento="2023-09-29"))
-        session.add(Album(nombre="I Disagree", artista="Poppy", genero="Nu Metal / Pop", fecha_lanzamiento="2020-01-10"))
-        session.add(Album(nombre="The Black Parade", artista="My Chemical Romance", genero="Emo / Rock Opera", fecha_lanzamiento="2006-10-23"))
-        session.add(Album(nombre="Shame On Me", artista="Catch Your Breath", genero="Alt Metal", fecha_lanzamiento="2023-10-20"))
-        session.add(Album(nombre="The Hell We Create", artista="Fit For A King", genero="Metalcore", fecha_lanzamiento="2022-10-28"))
-        session.add(Album(nombre="I Let It In and It Took Everything", artista="Loathe", genero="Metalcore / Shoegaze", fecha_lanzamiento="2020-02-07"))
-        session.commit()
+    conn = mysql.connector.connect(**temp_config)
+    cursor = conn.cursor()
+    
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_config['database']}")
+    conn.close()
+
+    return mysql.connector.connect(**db_config)
+
+def iniciar_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS albumes (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo VARCHAR(255) NOT NULL,
+            artista VARCHAR(255) NOT NULL,
+            genero VARCHAR(255) NOT NULL,
+            fecha_lanzamiento DATE NOT NULL
+            )
+    """)
+
+    cursor.execute("SELECT COUNT(*) FROM albumes")
+    count = cursor.fetchone()[0]
+
+    if count == 0:
+        print("Cargando datos nuevos en MySQL...")
+        
+        datos = [
+            ("POST HUMAN: NEX GEN", "Bring Me The Horizon", "Alt Metal / Electronic", "2024-05-24"),
+            ("The Death of Peace of Mind", "Bad Omens", "Industrial / Metalcore", "2022-02-25"),
+            ("Scoring The End of the World", "Motionless in White", "Industrial Metal", "2022-06-10"),
+            ("Take Me Back To Eden", "Sleep Token", "Prog / Alt Metal", "2023-05-19"),
+            ("Paper Hearts", "Sleep Theory", "Alt Rock / Metal", "2023-09-29"),
+            ("I Disagree", "Poppy", "Nu Metal / Pop", "2020-01-10"),
+            ("The Black Parade", "My Chemical Romance", "Emo / Rock Opera", "2006-10-23"),
+            ("Shame On Me", "Catch Your Breath", "Alt Metal", "2023-10-20"),
+            ("The Hell We Create", "Fit For A King", "Metalcore", "2022-10-28"),
+            ("I Let It In and It Took Everything", "Loathe", "Metalcore / Shoegaze", "2020-02-07")
+        ]
+
+        cursor.executemany("""
+            INSERT INTO albumes (titulo, artista, genero, fecha_lanzamiento) 
+            VALUES (%s, %s, %s, %s)
+        """, datos)
+        
+        conn.commit()
+        print("Datos insertados correctamente.")
+
+    cursor.close()
+    conn.close()
+
+def obtener_albumes():
+    iniciar_db() 
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True) 
+    
+    cursor.execute("SELECT * FROM albumes")
+    resultados = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+    return resultados
